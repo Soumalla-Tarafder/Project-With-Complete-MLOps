299d2cff373ba93e2ef8177ff6eafcac9175fc2d
soumalla <soumallatarafder@gmail.com>
2024-01-20 13:25:58 +0530

    all component structure created

---
+++ src/component/data_transformation.py
@@ -0,0 +1,32 @@
+import pandas as pd 
+import numpy as np 
+from src.logger.logging import logging
+from src.exception.exception import customexception
+import os,sys 
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
+
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+
+from src.utils.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    pass
+
+class DataTransformation:
+    def __init__(self):
+        pass
+    def initiate_data_ingestion(self):
+        try:
+            pass
+        except Exception as e:
+            logging.info(e)
+            raise customexception(e,sys)
+
+
